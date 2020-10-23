c00694a43fbc8405b553bc4eb8647b9560ea76e3
NivRichter <nivrichter@gmail.com>
2020-10-23 15:49:07 +0300

    Updates

---
+++ precache-manifest.ea15176cb9b238f8700ae515d42400d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca4bb375e2ea399a5ec6e59f2eb6974",
+    "url": "/parks/index.html"
+  },
+  {
+    "revision": "943b14233e3c04573f48",
+    "url": "/parks/static/css/2.28f42e79.chunk.css"
+  },
+  {
+    "revision": "c1a6d8aefb42e0de3acb",
+    "url": "/parks/static/css/main.41e60afc.chunk.css"
+  },
+  {
+    "revision": "943b14233e3c04573f48",
+    "url": "/parks/static/js/2.acb3a434.chunk.js"
+  },
+  {
+    "revision": "4b9783a61bdd4b7ec1da709e061f9f5e",
+    "url": "/parks/static/js/2.acb3a434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1a6d8aefb42e0de3acb",
+    "url": "/parks/static/js/main.8d76b707.chunk.js"
+  },
+  {
+    "revision": "a33269bbad01a4b74bd8",
+    "url": "/parks/static/js/runtime-main.5e4137ad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/parks/static/media/logo.5d5d9eef.svg"
+  }
+]);
